451031d0e5ec372c58a5e2baa953ba145da7fcab
Imran hassan <72485040+imran123509@users.noreply.github.com>
2022-01-05 10:32:39 +0530

    Add files via upload

---
+++ Goldman such question/Question(12) Squares in N*N Chessboard.cpp
@@ -0,0 +1,25 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+  public:
+    long long squaresInChessBoard(long long n) {
+        return (n*(n+1)*((2*n)+1))/6;
+    }
+};
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.squaresInChessBoard(N) << endl;
+    }
+    return 0;
+}
+
+//https://practice.geeksforgeeks.org/problems/squares-in-nn-chessboard1801/1#
